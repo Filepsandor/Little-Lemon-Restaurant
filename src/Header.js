4e3971299ca033b1824b86d738f4d96fd6ef0152
Filepsandor <sandorfilep1@gmail.com>
2024-07-29 17:19:44 +0200

    add css

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,23 @@
+import React from "react";
 import logo from "./logo.png";
+import "./Header.css";
 
 function Header() {
   return (
-    <header>
-      <img src={logo} alt="Logo" />
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" />
+      </div>
+      <nav className="desktop-navigation">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#menu">Menu</a></li>
+          <li><a href="#reservation">Reservation</a></li>
+          <li><a href="#order-online">Order online</a></li>
+          <li><a href="#login">Log in</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
